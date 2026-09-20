d26f745a63e6f2f9de950a2f4353dde128a24ec6
hahalu-ai <x25lu@uwaterloo.ca>
2026-09-20 00:08:49 -0400

    Refresh PWA cache for calendar update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "weight-loss-v2";
+const CACHE = "weight-loss-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
