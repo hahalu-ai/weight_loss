48bc742113a77146962e45d139df9617073c215f
hahalu-ai <x25lu@uwaterloo.ca>
2026-09-20 02:05:19 -0400

    Refresh cache for meal logging update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "weight-loss-v3";
+const CACHE = "weight-loss-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
