e98401f0ccef9f9485b699bcc136cec946d67857
hahalu-ai <x25lu@uwaterloo.ca>
2026-09-20 02:33:09 -0400

    Refresh cache for gallery upload fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "weight-loss-v5";
+const CACHE = "weight-loss-v6";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
